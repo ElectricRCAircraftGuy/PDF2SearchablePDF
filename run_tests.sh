5094b09134265025726879ebbe920c41ed5fcb9e
Gabriel Staples <ercaguy@gmail.com>
2020-12-27 16:09:01 -0800

    Add run_tests.sh

---
+++ run_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pdf2searchablepdf test_imgs
+pdf2searchablepdf test_pdfs/test1.pdf
+pdf2searchablepdf test_pdfs/Wikipedia_pdf_screenshot.png
+pdf2searchablepdf test_pdfs/Wikipedia_pdf_screenshot.pdf
+
