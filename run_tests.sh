22e41846ba273bda3693e60942ade5e7ce2fc363
Gabriel Staples <ercaguy@gmail.com>
2020-12-27 16:36:15 -0800

    Update run_tests.sh, & run latest tests
    This also commits all updated *_searchable.pdf files herein.

--- run_tests.sh
+++ run_tests.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-pdf2searchablepdf test_imgs
-echo -e "\n\n"
+test() {
+    echo -e ">>> Running 'pdf2searchablepdf \"$1\"' <<<"
+    pdf2searchablepdf "$1"
+    echo -e "\n\n\n"
+}
 
-pdf2searchablepdf test_pdfs/test1.pdf
-echo -e "\n\n"
+test test_imgs
+test test_pdfs/test1.pdf
+test test_pdfs/test1_edited_w_foxit.pdf
+# test test_pdfs/Wikipedia_pdf_screenshot.png
+test test_pdfs/Wikipedia_pdf_screenshot.pdf
 
-pdf2searchablepdf test_pdfs/test1_edited_w_foxit.pdf
-echo -e "\n\n"
-
-# pdf2searchablepdf test_pdfs/Wikipedia_pdf_screenshot.png
-# echo -e "\n\n"
-
-pdf2searchablepdf test_pdfs/Wikipedia_pdf_screenshot.pdf
-echo -e "\n\n"
 
