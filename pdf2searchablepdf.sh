6445ef86b35cacc80f4ee25d8575b71b119ea6d0
Gabriel Staples <ercaguy@gmail.com>
2020-12-27 19:04:20 -0800

    Update help menu in pdf2searchablepdf.sh; update run_tests.sh

--- pdf2searchablepdf.sh
+++ pdf2searchablepdf.sh
@@ -6,16 +6,7 @@
 # - Source code: https://github.com/ElectricRCAircraftGuy/PDF2SearchablePDF
 
 # Test runs:
-#   Convert test pdfs:
-#     pdf2searchablepdf ./test_pdfs/test1.pdf
-#     pdf2searchablepdf ./test_pdfs/Wikipedia_pdf_screenshot.pdf
-#   Convert a whole directory of images into a single, searchable pdf:
-#     pdf2searchablepdf test_imgs/
-#   Print help menu
-#     pdf2searchablepdf -h
-#     pdf2searchablepdf -?
-#   Print version
-#     pdf2searchablepdf -v
+# See "run_tests.sh"
 
 start=$SECONDS
 
@@ -29,46 +20,66 @@ AUTHOR="Gabriel Staples"
 # See: https://stackoverflow.com/questions/12845638/how-do-i-change-the-decimal-separator-in-the-printf-command-in-bash/12845640#12845640.
 export LC_NUMERIC="en_US.UTF-8"
 
+
+SCRIPT_NAME="$(basename "$0")"
+VERSION_SHORT_STR="pdf2searchablepdf (run as '$SCRIPT_NAME') version $VERSION"
+VERSION_LONG_STR="\
+$VERSION_SHORT_STR
+Author = $AUTHOR
+See '$SCRIPT_NAME -h' for more info.
+"
+
+# Note that the ISO 639-2 language code requirement is mentioned in the man pages here:
+# https://github.com/tesseract-ocr/tesseract/blob/master/doc/tesseract.1.asc
+HELP_STR="\
+$VERSION_SHORT_STR
+
+Purpose: convert \"input.pdf\" to a searchable PDF named \"input_searchable.pdf\"
+by using tesseract to perform OCR (Optical Character Recognition) on the PDF.
+
+Usage:    '$SCRIPT_NAME <input.pdf | dir_of_imgs> [lang]' = if the 1st argument is
+            to an input pdf, then convert input.pdf to input_searchable.pdf using language
+            \"lang\" for OCR. Otherwise, if the 1st argument is a path to a directory containing
+            a bunch of images, convert the whole directory of images into a single PDF, using
+            language \"lang\" for OCR!
+          '$SCRIPT_NAME'    = print help menu
+          '$SCRIPT_NAME -h' = print help menu
+          '$SCRIPT_NAME -?' = print help menu
+          '$SCRIPT_NAME -v' = print author & version
+
+Examples: '$SCRIPT_NAME mypdf.pdf deu' = convert mypdf.pdf to a searchable PDF, using
+            German text OCR, or
+          '$SCRIPT_NAME mypdf.pdf' for English text OCR (the default).
+          '$SCRIPT_NAME dir_of_imgs' = convert all images in this directory, \"dir_of_imgs\",
+            to a single, searchable PDF.
+
+[lang]
+    The optional [lang] argument allows you to perform OCR in your language of choice.
+    This parameter will be passed on to tesseract. You must use ISO 639-2 3-letter language
+    codes. Ex: \"deu\" for German, \"dan\" for Danish, \"eng\" for English, etc.
+    See the \"LANGUAGES\" section of the tesseract man pages ('man tesseract') for a
+    complete list. If the [lang] parameter is not given, English will be used by default.
+    If you don't have a desired language installed, it may be obtained from one of the
+    following 3 repos (see tesseract man pages for details):
+      - https://github.com/tesseract-ocr/tessdata_fast
+      - https://github.com/tesseract-ocr/tessdata_best
+      - https://github.com/tesseract-ocr/tessdata
+    To install a new language, simply download the respective \"*.traineddata\" file from one of
+    the 3 repos above and copy it to your tesseract installation's \"tessdata\" directory.
+    See \"Post-Install Instructions\" here:
+    https://github.com/tesseract-ocr/tessdoc/blob/master/Compiling-%E2%80%93-GitInstallation.md#post-install-instructions
+
+Tesseract Wiki: https://github.com/tesseract-ocr/tesseract/wiki.
+
+Source code: https://github.com/ElectricRCAircraftGuy/PDF2SearchablePDF
+"
+
 print_help() {
-    echo 'Purpose: convert "input.pdf" to a searchable PDF named "input_searchable.pdf"'
-    echo 'by using tesseract to perform OCR (Optical Character Recognition) on the PDF.'
-    echo 'Usage:    `pdf2searchablepdf <input.pdf | dir_of_imgs> [lang]` = if the 1st argument is'
-    echo '            to an input pdf, then convert input.pdf to input_searchable.pdf using language'
-    echo '            "lang" for OCR. Otherwise, if the 1st argument is a path to a directory containing'
-    echo '            a bunch of images, convert the whole directory of images into a single PDF, using'
-    echo '            language "lang" for OCR!'
-    echo '          `pdf2searchablepdf`    = print help menu'
-    echo '          `pdf2searchablepdf -h` = print help menu'
-    echo '          `pdf2searchablepdf -?` = print help menu'
-    echo '          `pdf2searchablepdf -v` = print author & version'
-    echo 'Examples: `pdf2searchablepdf mypdf.pdf deu` = convert mypdf.pdf to a searchable PDF, using'
-    echo '            German text OCR, or'
-    echo '          `pdf2searchablepdf mypdf.pdf` for English text OCR (the default).'
-    echo '          `pdf2searchablepdf dir_of_imgs` = convert all images in this directory, "dir_of_imgs",'
-    echo '            to a single, searchable PDF.'
-    echo '  The optional [lang] argument allows you to perform OCR in your language of choice.'
-    echo '  This parameter will be passed on to tesseract. You must use ISO 639-2 3-letter language'
-    # Note that the ISO 639-2 language code requirement is mentioned in the man pages here:
-    # https://github.com/tesseract-ocr/tesseract/blob/master/doc/tesseract.1.asc
-    echo '  codes. Ex: "deu" for German, "dan" for Danish, "eng" for English, etc.'
-    echo '  See the "LANGUAGES" section of the tesseract man pages (`man tesseract`) for a'
-    echo '  complete list. If the [lang] parameter is not given, English will be used by default.'
-    echo '  If you don'"'"'t have a desired language installed, it may be obtained from one of the'
-    echo '  following 3 repos (see tesseract man pages for details):'
-    echo '    - https://github.com/tesseract-ocr/tessdata_fast'
-    echo '    - https://github.com/tesseract-ocr/tessdata_best'
-    echo '    - https://github.com/tesseract-ocr/tessdata'
-    echo '  To install a new langauge, simply download the respective "*.traineddata" file from one of'
-    echo '  the 3 repos above and copy it to your tesseract installation'"'"'s "tessdata" directory.'
-    echo '  See "Post-Install Instructions" here:'
-    echo '  https://github.com/tesseract-ocr/tesseract/wiki/Compiling-%E2%80%93-GitInstallation.'
-    echo 'Source code: https://github.com/ElectricRCAircraftGuy/PDF2SearchablePDF'
+    echo "$HELP_STR" | less -RFX
 }
 
 print_version() {
-    echo "pdf2searchablepdf version $VERSION"
-    echo "Author = $AUTHOR"
-    echo 'See `pdf2searchablepdf -h` for more info.'
+    echo "$VERSION_LONG_STR"
 }
 
 parse_args() {
--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-test() {
-    echo -e ">>> Running 'pdf2searchablepdf \"$1\"' <<<"
-    pdf2searchablepdf "$1"
+test_pdf2searchablepdf() {
+    echo -e ">>> Running 'pdf2searchablepdf $@' <<<"
+    pdf2searchablepdf "$@"
     echo -e "\n\n\n"
 }
 
@@ -12,10 +12,27 @@ if [ ! -f ~/bin/pdf2searchablepdf ]; then
     ./install.sh
 fi
 
-test tests/imgs
-test tests/pdfs/test1.pdf
-test tests/pdfs/test1_edited_w_foxit.pdf
-# test tests/pdfs/Wikipedia_pdf_screenshot.png
-test tests/pdfs/Wikipedia_pdf_screenshot.pdf
+# ===========
+# Tests
+# ===========
+
+# Print help menu
+test_pdf2searchablepdf -h
+test_pdf2searchablepdf -?
+
+# Print version
+test_pdf2searchablepdf -v
+
+# # Convert some PDFs into searchable PDFs
+# test_pdf2searchablepdf tests/pdfs/test1.pdf
+# test_pdf2searchablepdf tests/pdfs/test1_edited_w_foxit.pdf
+# test_pdf2searchablepdf tests/pdfs/Wikipedia_pdf_screenshot.pdf
+
+# # Convert a whole directory of images into a single, searchable pdf:
+# test_pdf2searchablepdf tests/imgs
+
+# # Convert a single image (NOT IMPLEMENTED YET)
+# # test_pdf2searchablepdf tests/pdfs/Wikipedia_pdf_screenshot.png
+
 
 
