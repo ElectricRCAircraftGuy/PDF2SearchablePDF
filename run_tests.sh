fff50358aed64850f9b51bf240153bf1212b5d21
Gabriel Staples <ercaguy@gmail.com>
2020-12-27 16:19:18 -0800

    run_tests.sh: update tests; add another PDF to test

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 pdf2searchablepdf test_imgs
+echo -e "\n\n"
+
 pdf2searchablepdf test_pdfs/test1.pdf
-pdf2searchablepdf test_pdfs/Wikipedia_pdf_screenshot.png
+echo -e "\n\n"
+
+pdf2searchablepdf test_pdfs/test1_edited_w_foxit.pdf
+echo -e "\n\n"
+
+# pdf2searchablepdf test_pdfs/Wikipedia_pdf_screenshot.png
+# echo -e "\n\n"
+
 pdf2searchablepdf test_pdfs/Wikipedia_pdf_screenshot.pdf
+echo -e "\n\n"
 
